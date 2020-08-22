0cd3b7bc0f465288ddc21fd65a5c2d13b7d342e1
Matthew Lau <matthewlau@matthews-mbp.mynet>
2020-08-22 11:04:39 +0100

    add fetch todos

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,10 +4,31 @@ import { PageHeader } from "antd";
 import { Card, Button, Input } from "antd";
 import "antd/dist/antd.css";
 import { Layout, Spin } from "antd";
+import axios from "axios";
 
 const { Content } = Layout;
 
 const HomePage = () => {
+  const loadingState = false;
+  const [todos, setTodos] = useState([]);
+  const [loadingComplete, setloadingComplete] = useState(loadingState);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  async function fetchTodos() {
+    try {
+      const res = await axios.get(
+        `https://j7sphw5bq4.execute-api.us-east-1.amazonaws.com/test/todos`
+      );
+      setTodos(res.data.Items);
+      setloadingComplete({ loadingComplete: true });
+    } catch (err) {
+      console.log("error fetching todos");
+    }
+  }
+
   return (
     <div>
       <Content style={{ padding: "0 50px" }}>
@@ -19,6 +40,17 @@ const HomePage = () => {
             style={styles.header}
           />
         </div>
+        {loadingComplete ? (
+          <div>
+            {todos.map((todo, index) => (
+              <Card key={todo.id.S} title={todo.name.S} style={{ width: 300 }}>
+                <p>{todo.description.S}</p>
+              </Card>
+            ))}
+          </div>
+        ) : (
+          <Spin />
+        )}
       </Content>
     </div>
   );
