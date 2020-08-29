0d3080d21d4cc49d2dddb37f05a708778465541c
Matthew Lau <matthewlau@matthews-mbp.mynet>
2020-08-29 19:43:33 +0100

    added get todo by id js

---
+++ deploy/lambdas/getTodoById.js
@@ -0,0 +1,44 @@
+// Load the AWS SDK for Node.js
+const AWS = require("aws-sdk");
+
+// Set the region
+AWS.config.update({ region: "us-east-1" });
+
+// Create DynamoDB service object
+const ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
+
+exports.handler = (event, context, callback) => {
+  const id = event.path.split("/")[2];
+  let responseCode = 200;
+  let responseBody = "";
+  const params = {
+    Key: {
+      id: {
+        S: id
+      }
+    },
+    TableName: "todos"
+  };
+  ddb.getItem(params, function(err, data) {
+    if (err) {
+      responseCode = 500;
+      responseBody = err;
+    } else {
+      if (data.Item) {
+        responseBody = data;
+      } else {
+        responseCode = 404;
+        responseBody = "Data not found";
+      }
+    }
+    const response = {
+      statusCode: responseCode,
+      headers: {
+        "content-type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify(responseBody)
+    };
+    callback(null, response);
+  });
+};
