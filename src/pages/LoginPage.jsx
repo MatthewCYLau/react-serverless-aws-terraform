b42a130801392475844c651eb6dfddca06bb2fff
Matthew Lau <matthewlau@matthews-mbp.mynet>
2020-09-19 14:02:50 +0100

    add Login page

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { PageHeader } from "antd";
+import { Layout } from "antd";
+
+const { Content } = Layout;
+
+const LoginPage = () => {
+  return (
+    <div>
+      <Content style={{ padding: "0 50px" }}>
+        <div className="site-layout-content">
+          <PageHeader
+            className="site-page-header"
+            title="Login"
+            style={styles.header}
+          />
+        </div>
+      </Content>
+    </div>
+  );
+};
+
+const styles = {
+  input: {
+    margin: "10px 0"
+  },
+  submit: {
+    margin: "10px 0",
+    marginBottom: "20px"
+  },
+  header: {
+    paddingLeft: "0px"
+  }
+};
+
+export default LoginPage;
--- src/routers/Routes.jsx
+++ src/routers/Routes.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import AboutPage from "../pages/AboutPage";
+import LoginPage from "../pages/LoginPage";
 import NotFoundPage from "../pages/NotFoundPage";
 import EditTodoPage from "../pages/EditTodoPage";
 
@@ -9,6 +10,7 @@ const Routes = () => {
     <section>
       <Switch>
         <Route exact path="/about" component={AboutPage} />
+        <Route exact path="/login" component={LoginPage} />
         <Route exact path="/edit/:id" component={EditTodoPage} />
         <Route component={NotFoundPage} />
       </Switch>
