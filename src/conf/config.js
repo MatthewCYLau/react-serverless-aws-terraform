bad4f659fcbbb18333391e59040018fdb236e91f
Matthew Lau <matthewlau@matthews-mbp.mynet>
2020-09-19 16:21:33 +0100

    fix config

--- src/conf/config.js
+++ src/conf/config.js
@@ -1,4 +1,4 @@
 export default {
-  apiEndpoint = process.env.REACT_APP_API_ENDPOINT
+  apiEndpoint: process.env.REACT_APP_API_ENDPOINT
   // apiEndpoint: "https://17kl2drmmc.execute-api.us-east-1.amazonaws.com/dev"
 };
