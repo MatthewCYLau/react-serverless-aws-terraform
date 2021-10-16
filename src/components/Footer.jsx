cf83bf20c06bd734b72ec9d56078358c9b586cae
Matthew Lau <lau.cy.matthew@gmail.com>
2021-10-16 15:05:03 +0100

    feat: Updated footer year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Layout } from "antd";
 const { Footer } = Layout;
 
 const AppFooter = () => (
-  <Footer style={{ textAlign: "center" }}>Matthew Lau ©2020</Footer>
+  <Footer style={{ textAlign: "center" }}>Matthew Lau ©2021</Footer>
 );
 
 export default AppFooter;
