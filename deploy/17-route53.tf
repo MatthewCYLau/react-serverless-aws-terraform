dbd80a0b527c0c27350ded217936b55cc5bbb577
Matthew Lau <lau.cy.matthew@gmail.com>
2021-10-18 23:00:18 +0100

    feat: added route 53 terraform for A records

---
+++ deploy/17-route53.tf
@@ -0,0 +1,23 @@
+resource "aws_route53_record" "root-a" {
+  zone_id = "Z07982832AOOQSSCIJNDL"
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.root_s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.root_s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "www-a" {
+  zone_id = "Z07982832AOOQSSCIJNDL"
+  name    = "www.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.www_s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.www_s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
