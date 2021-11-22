a262a7113a33d3c4d3720c46352858be5afcf4af
Noor Zaidi (Credera) <noor.zaidi@credera.co.uk>
2021-11-22 12:27:25 +0000

    about page paragraph has been added

--- src/conf/config.js
+++ src/conf/config.js
@@ -1,12 +1,12 @@
 export default {
   apiGateway: {
     REGION: "us-east-1",
-    URL: process.env.REACT_APP_API_ENDPOINT
+    URL: "https://yilnn2sp51.execute-api.us-east-1.amazonaws.com/staging",
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: process.env.REACT_APP_USER_POOL_ID,
-    APP_CLIENT_ID: process.env.REACT_APP_APP_CLIENT_ID,
-    IDENTITY_POOL_ID: process.env.REACT_APP_IDENTITY_POOL_ID
-  }
+    USER_POOL_ID: "us-east-1_MQjRuUSu9",
+    APP_CLIENT_ID: "e57hmhni5h6uf8hrq8gk4h805",
+    IDENTITY_POOL_ID: "us-east-1:4c5e8c2b-29ce-465e-81c3-0e73e3f2ffd1",
+  },
 };
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { PageHeader } from "antd";
 import { Layout } from "antd";
+import { Typography, Space } from 'antd';
 
 const { Content } = Layout;
+const { Text, Link } = Typography;
 
 const AboutPage = () => {
   return (
@@ -14,6 +16,18 @@ const AboutPage = () => {
             title="About"
             style={styles.header}
           />
+          <Space direction="vertical">
+          <Text>We are Mattdera, a small tech startup in Manchester. Our goal — to help you get <Text strong>organised!!!</Text></Text>
+          <Text>Our revolutionary app will help you achieve all your hopes and dreams and help you makes loads of money.🤑</Text>
+          <Text>Some of our amazing features include</Text>
+          <ul>
+            <li><Text>You can add to-do items.</Text></li>
+            <li><Text>Add descriptions for your tasks.</Text></li>
+            <li><Text>Comment on your friends goals.</Text></li>
+            <li><Text>Like others comments.</Text></li>
+          </ul>
+          <Text>Contact us about any additional features you want to see and give our repo a star on <Link href="https://github.com/MatthewCYLau/react-serverless-aws-terraform" target="_blank">Github.</Link></Text>
+          </Space>
         </div>
       </Content>
     </div>
