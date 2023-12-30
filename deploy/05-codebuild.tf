15e18230d1a19e43ee4dd7d7fb90c95a170bfc1d
matthew.lau <matthew.lau@credera.co.uk>
2023-12-30 12:12:02 +0000

    chore: increase build timeout

--- deploy/05-codebuild.tf
+++ deploy/05-codebuild.tf
@@ -1,7 +1,7 @@
 resource "aws_codebuild_project" "app" {
   name          = var.app_name
   description   = var.app_name
-  build_timeout = "5"
+  build_timeout = "30"
   service_role  = aws_iam_role.codebuild_role.arn
 
   artifacts {
